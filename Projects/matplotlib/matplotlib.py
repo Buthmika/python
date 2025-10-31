549647b3dd6f03d337479beeb190ee1d2d9aa315
Dampitige DBM <buthmikamenushan@gmail.com>
2025-10-31 14:00:05 +0530

    Update matplotlib script to show category plots
    Commented out previous line and scatter plots, and added bar and scatter plots for categorical data using subplots. The script now visualizes 'group A', 'group B', and 'group C' with corresponding values, sets a figure title, and saves the output as 'test.png'.

--- Projects/matplotlib/matplotlib.py
+++ Projects/matplotlib/matplotlib.py
@@ -7,13 +7,26 @@ import matplotlib.pyplot as plt
 # plt.scatter(x2,y2)
 # plt.show()
 import numpy as np
-x=np.arange(0,100,2)
-y1=x*2+3
-y2=np.power(x,2)
-plt.plot(x,y1,'r--',label='x vs y1')
-plt.plot(x,y2,'g+',label='x vs y2')
-plt.legend()
-plt.xlabel('x')
-plt.ylabel('y')
-plt.savefig('newImage.png')
+# x=np.arange(0,100,2)
+# y1=x*2+3
+# y2=np.power(x,2)
+# plt.plot(x,y1,'r--',label='x vs y1')
+# plt.plot(x,y2,'g+',label='x vs y2')
+# plt.legend()
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.savefig('newImage.png')
+# plt.show()
+names=['group A','group B','group C']
+values=[1,10,100]
+plt.figure(figsize=(12,5))
+plt.subplot(133)
+plt.bar(names,values)
+plt.subplot(132)
+plt.scatter(names,values)
+plt.subplot(131)
+plt.bar(names,values)
+
+plt.suptitle('Category Plotting')
+plt.savefig('test.png')
 plt.show()
